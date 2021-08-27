9a1487286e1a3c0a51039049c729bec54ad0905a
devandtravel <devandtravel@gmail.com>
2021-08-27 15:04:09 +0300

    add: layout, styles

--- main.js
+++ main.js
@@ -5,5 +5,28 @@ fetch(
   .then(data => setHeroes(data))
 
 function setHeroes({ squadName, homeTown, formed, members }) {
-  document.body.insertAdjacentHTML('afterbegin', `<h1>${squadName}</h1>`)
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `<h1>${squadName}</h1>
+    <h2>Hometown: ${homeTown} // Formed: ${formed}</h2>
+    <div class="heroes">${setMembers(members)}</div>`
+  )
+}
+
+function setMembers(members) {
+  return members
+    .map(
+      member =>
+        `<div>
+          <h3>${member.name}</h3>
+                <p>Secret identity: ${member.secretIdentity}</p>
+                <p>Age: ${member.age}</p>
+                <p>Superpowers:</p>
+                <ul>
+          ${member.powers.map(power => `<li>${power}</li>`).join(' ')}
+                </ul>
+        </div>
+      `
+    )
+    .join(' ')
 }
